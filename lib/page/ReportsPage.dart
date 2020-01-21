ddf81853a9651941fe175613c506b0fe23742250
Samuel Fry <sam@zingbooks.com>
2020-01-21 09:39:45 -0700

    Add reports page.

--- lib/page/ReportsPage.dart
+++ lib/page/ReportsPage.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:project_apraxia/data/WsdReport.dart';
 
-
 class ReportsPage extends StatefulWidget {
   final WsdReport wsdReport;
+
   ReportsPage(this.wsdReport, {Key key}) : super(key: key);
 
   @override
@@ -12,17 +12,78 @@ class ReportsPage extends StatefulWidget {
 
 class _ReportsPageState extends State<ReportsPage> {
   WsdReport wsdReport;
+  bool loading;
 
-  _ReportsPageState(this.wsdReport);
+  _ReportsPageState(WsdReport wsdReport) {
+    this.wsdReport = wsdReport;
+    loading = false;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    calculateWSDs();
+  }
+
+  void calculateWSDs() {
+    setState(() {
+      loading = true;
+    });
+    Future.delayed(new Duration(seconds: 1), () {
+      setState(() {
+        loading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Reports Page"),
-      ),
-      body: Center(child: Text("Reports Page"))
-    );
+    return loading
+        ? Scaffold(
+            appBar: AppBar(
+              title: Text("Reports Page"),
+            ),
+            body: Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                  CircularProgressIndicator(),
+                  Text("Loading Reports...")
+                ])))
+        : Scaffold(
+            appBar: AppBar(
+              title: Text("Reports Page"),
+            ),
+            body: Column(children: [
+              Table(children: [
+                TableRow(children: [Text("Word"), Text("Calculated WSD")]),
+                TableRow(children: [Text("Gingerbread"), Text("320")]),
+                TableRow(children: [
+                  Text("Constitution"),
+                  Text("320"),
+                ]),
+                TableRow(children: [Text("Flattering"), Text("320")]),
+                TableRow(children: [
+                  Text("Harmonica"),
+                  Text("320"),
+                ]),
+                TableRow(children: [Text("Jabbering"), Text("320")]),
+                TableRow(children: [
+                  Text("Spaghetti"),
+                  Text("320"),
+                ]),
+                TableRow(children: [Text("Stethoscope"), Text("320")]),
+                TableRow(children: [
+                  Text("Thickening"),
+                  Text("320"),
+                ]),
+                TableRow(children: [Text("Volcano"), Text("320")]),
+                TableRow(children: [
+                  Text("Zippering"),
+                  Text("320"),
+                ])
+              ])
+            ]));
   }
-
 }
