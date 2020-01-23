ab54e9741cebd0ead9f050e3e7e3f1382c6c6450
Samuel Fry <sam@zingbooks.com>
2020-01-22 22:17:27 -0700

    Fix android setAmbiance return value, change wsdCalculator to use "calculateAmbiance" instead of "calculateAmbience"

--- lib/controller/WSDCalculator.dart
+++ lib/controller/WSDCalculator.dart
@@ -5,13 +5,13 @@ import 'package:project_apraxia/model/Attempt.dart';
 class WSDCalculator extends IWSDCalculator {
   static const channel = const MethodChannel("wsdCalculator");
   
-  Future<double> calculateWSD(String filename, {double ambienceThreshold}) async {
+  Future<double> calculateWSD(String filename, {double ambianceThreshold}) async {
       return await channel.invokeMethod("calculateWSD", [filename]); 
   }
 
   @override
   Future<String> setAmbiance(String fileName) async {
-    return await channel.invokeMethod("calculateAmbience", [fileName]);
+    return await channel.invokeMethod("calculateAmbiance", [fileName]);
   }
 
   @override
@@ -23,30 +23,4 @@ class WSDCalculator extends IWSDCalculator {
   Future<List<double>> getAmplitudes(String fileName) async {
     return await channel.invokeMethod("getAmplitude", [fileName]);
   }
-  
-//  Future<double> calculateWSD(String filename, {double ambienceThreshold}) async {
-//    if (Platform.isAndroid){
-//      return await channel.invokeMethod("calculateWSD", [filename]);
-//    }
-//    else if (Platform.isIOS){
-//      ambienceThreshold ??= await calculateAmbience(filename);
-//      return await channel.invokeMethod("calculateWSD", <String, dynamic>{
-//        'fileName': filename,
-//        'ambienceThreshold': ambienceThreshold,
-//      });
-//    }
-//
-//    throw PlatformException(message: "Platform Not Supported", code: null);
-//  }
-//
-//  Future<double> calculateAmbience(String filename) async {
-//    // print("in calculate wsd hello");
-//    double ambience = await channel.invokeMethod("calculateAmbience", [filename]);
-//    return ambience;
-//    // return await channel.invokeMethod("calculateAmbience", [filename]);
-//  }
-//
-//  Future<List<double>> getAmplitude(String filename) async {
-//    return await channel.invokeMethod("getAmplitude", [filename]);
-//  }
 }
